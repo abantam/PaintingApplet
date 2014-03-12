491f6c8a8946ab3e486b57b13ba59c2e63253e70
abantam <abantam.9@gmail.com>
2014-03-13 02:17:34 +0900

    2014/3/13 ver1.0

---
+++ PaintingApplet/src/pa/Main.java
@@ -0,0 +1,86 @@
+package pa;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JApplet;
+
+public class Main extends JApplet implements MouseListener, MouseMotionListener {
+	
+	int status;
+	
+	int scrSize = 700;
+	
+	int x;
+	int y;
+	
+	@Override
+	public void init() {
+		x = 0;
+		y = 0;
+		setSize(scrSize, scrSize);
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		
+		status = 0;
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		if(status == 1) {
+			g.setColor(Color.black);
+			g.fillOval(x, y, 10, 10);
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		Point p = e.getPoint();
+		x = (int) p.getX();
+		y = (int) p.getY();
+		status = 1;
+		
+		repaint();
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
